5d5622416e1690d1258776ef50b1011e08da9628
Bishajit Lodh <lodh@Bishajits-MBP.fios-router.home>
2023-11-08 23:10:30 -0500

    first handsOn

---
+++ handsOn/src/package_main/Main.java
@@ -0,0 +1,14 @@
+package package_main;
+
+public class Main { //main class
+
+    //inside this class we have our main method
+    public static void main(String[] args) { //main method
+        //public means it is available from other parts of our program
+        //main method always static
+        // void mean not gonna return a value
+
+        System.out.println("Hello World");
+
+    }
+}
