ce59e41040ba30583b96a7cbc1e8ad0bc39d3fd7
Bishajit Lodh <lodh@Bishajits-MBP.fios-router.home>
2023-11-11 22:29:21 -0500

    comments in main

--- handsOn/src/package_main/Main.java
+++ handsOn/src/package_main/Main.java
@@ -7,16 +7,13 @@ public class Main { //main class
         //public means it is available from other parts of our program
         //main method always static
         // void mean not gonna return a value
+        // byte age = 30;
+        //  long viewCount = 3_123_143_124L; //Wont save as a int so save it as a long but even as long too big so have to add L
+        // dnt use lowercase l because looks like a 1 so use capital L
+        // float price = 10.99F; //You need to add f or F at the end or it automatically saves as a Double
+        // boolean isEligible = true;
+        // System.out.println("Hello World");
 
-        byte age = 30;
-        long viewCount = 3_123_143_124L; //Wont save as a int so save it as a long but even as long too big so have to add L
-        //dnt use lowercase l because looks like a 1 so use capital L
-        float price = 10.99F; //You need to add f or F at the end or it automatically saves as a Double
-        boolean isEligible = true;
-
-
-
-        System.out.println("Hello World");
 
     }
 }
